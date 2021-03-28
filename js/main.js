d577ee5efc742b86cb9181e3685942a4d6e49ede
Genn <intval@i.ua>
2021-03-28 21:42:19 +0300

    added-on-index-switcher

--- js/main.js
+++ js/main.js
@@ -2,16 +2,21 @@ let shch = {
     show: function () {
         let bodyMain = document.querySelector('body');
         let bodyMainClass = bodyMain.getAttribute('class');
-        if (bodyMainClass === 'overno') {
-            bodyMain.setAttribute('class', 'overyes');
-        } else {
-            bodyMain.setAttribute('class', 'overno');
-        }
+        if (bodyMainClass === 'overno') bodyMain.setAttribute('class', 'overyes');
+        else bodyMain.setAttribute('class', 'overno');
+    },
+    extender: function () {
+        if (this.getAttribute('class') === 'maindirrection switcher') this.setAttribute('class', 'maindirrection switcher show')
+        else this.setAttribute('class', 'maindirrection switcher')
     },
     burger: function () {
         let visible = document.querySelector('.burger');
         visible.addEventListener('click', shch.show);
-        let switcher = document.querySelector('.switcher');
+        let switchers = document.querySelectorAll('.switcher');
+        let i = 0;
+        for (i; i < switchers.length; i++) {
+            document.querySelectorAll('.switcher')[i].addEventListener('click', shch.extender);
+        }
     }
 }
 window.addEventListener('load', shch.burger);

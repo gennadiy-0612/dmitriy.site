6795cc888b0bd8719eda1788cc6fd467c3c978de
Genn <intval@i.ua>
2021-03-29 22:30:20 +0300

    BackGround-ball-was-added

--- js/main.js
+++ js/main.js
@@ -9,14 +9,38 @@ let shch = {
         if (this.getAttribute('class') === 'maindirrection switcher') this.setAttribute('class', 'maindirrection switcher show')
         else this.setAttribute('class', 'maindirrection switcher')
     },
+    initPoint: 0,
     activeLight: function (e) {
         e.stopPropagation();
         let currentPosition = this.offsetLeft;
         let activeBack = document.querySelector('.listworks__blueball');
-        activeBack.setAttribute('style', 'left:' + currentPosition + 'px;');
-        document.querySelectorAll('.listworks__li')[3].offsetLeft
-        let posit = window.screenX;
-        console.log(posit);
+        shch.currPos = currentPosition - 10;
+        activeBack.setAttribute('style', 'left:' + (currentPosition - 10) + 'px;');
+        shch.initPoint = currentPosition - 10;
+        console.log(shch.initPoint + " " + currentPosition);
+    },
+    ainmated: function () {
+        var o = 1.0; // start opacity
+        var d = false; // direction. false = decrease, true = increase
+        var s = 0.02; // step
+        function anim() {
+            if (d == false) {
+                o -= s;
+                if (o < 0.0) {
+                    o = 0.0;
+                    d = true;
+                }
+            } else {
+                o += s;
+                if (o > 1.0) {
+                    o = 1.0;
+                    d = false;
+                }
+            }
+            document.querySelector('.listworks__blueball').style.opacity = o;
+        }
+
+        setInterval(anim, 1000 / 60); // 60 FPS.
     },
     burger: function () {
         let visible = document.querySelector('.burger');
@@ -30,6 +54,7 @@ let shch = {
         for (n; n < switchers.length; n++) {
             document.querySelectorAll('.listworks__li')[n].addEventListener('click', shch.activeLight);
         }
+        shch.ainmated();
     }
 }
 window.addEventListener('load', shch.burger);

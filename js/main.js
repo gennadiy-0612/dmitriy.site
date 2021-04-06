e07cdbfe2094f53ee154a14d16f46b98b6ace3d7
Genn <intval@i.ua>
2021-04-06 09:19:35 +0300

    1300px-wide-fixed

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ let shch = {
     count: 0,
     curr: 0,
     distance: 0,
+    startTop: 0,
+    currTop: 0,
+    distanceTop: 0,
     animBall: document.querySelector('.listworks__blueball'),
     activeLight: function (e) {
         if (shch.move) {
@@ -21,6 +24,8 @@ let shch = {
         shch.move = 1;
         shch.curr = this.offsetLeft - 10;
         shch.distance = shch.start - shch.curr;
+        // shch.currTop = this.offsetTop;
+        // shch.distanceTop = shch.currTop - shch.startTop;
         shch.distance < 0 ? shch.animated() : shch.animatedLeft();
     },
     animated: function () {
@@ -29,10 +34,20 @@ let shch = {
             shch.start += shch.count;
             shch.animBall.setAttribute('style', 'left:' + shch.start + 'px;');
             if (shch.start > shch.curr) {
+                // if (shch.currTop) {
+                //     shch.startTop += 1;
+                //     // shch.animBall.setAttribute('style:','top:' + shch.startTop + 'px;');
+                //     if (shch.startTop > shch.distanceTop) {
+                //         shch.startTop = shch.distanceTop;
+                //         console.log(shch.startTop);
+                //         console.log(shch.distanceTop);
+                //         return true;
+                //     }
+                //     shch.startTop = shch.currTop;
+                // }
                 clearInterval(shch.go);
                 shch.start = shch.curr;
                 shch.move = 0;
-                console.log(shch.move);
                 return false;
             }
         }
@@ -45,6 +60,10 @@ let shch = {
             shch.start -= shch.count;
             shch.animBall.setAttribute('style', 'left:' + shch.start + 'px;');
             if (shch.distance < 0) {
+                // if (shch.distanceTop) {
+                //     shch.startTop = shch.currTop;
+                //     console.log(shch.distanceTop)
+                // }
                 shch.move = 0;
                 shch.start = shch.curr;
                 shch.count = 0;
@@ -56,8 +75,11 @@ let shch = {
     },
     burger: function () {
         let visible = document.querySelector('.burger');
-        if ((window.location.pathname === "/pf/dmitriy.site/index.html") || (window.location.pathname === "/pf/dmitriy.site/")) {
-            visible.addEventListener('click', shch.show);
+        visible.addEventListener('click', shch.show);
+        shch.locate = {};
+        if (window.location.origin == 'https://p.cx.ua') shch.locate={index1:'/pf/dmitriy.site/',index2:'/pf/dmitriy.site/index.html'};
+        if (window.location.origin == 'http://localhost:81') shch.locate={index1:'/',index2:'index.html'};
+        if ((window.location.pathname == shch.locate.index1) || (window.location.pathname == shch.locate.index2)) {
             let logisticI = 0;
             let logisticSwitcher = document.querySelectorAll('.maindirrection.switcher');
             let logisticSwitcherCount = logisticSwitcher.length;
@@ -89,10 +111,10 @@ let shch = {
             document.querySelector('.videoSlidePlus').addEventListener('click', shch.slideVideo.Plus.bind(shch.slideVideo));
             document.querySelector('.videoSlideMinus').addEventListener('click', shch.slideVideo.Minus.bind(shch.slideVideo));
         }
-        // shch.ScrollDetect.firstEffect = new shch.ScrollDetect('.aboutus', 'appear', 1);
-        // window.addEventListener('scroll', shch.ScrollDetect.firstEffect.scrolling.bind(shch.ScrollDetect.firstEffect), false);
-        // shch.ScrollDetect.firstEffect = new shch.ScrollDetect('.keyprinciples', 'appear', 1);
-        // window.addEventListener('scroll', shch.ScrollDetect.firstEffect.scrolling.bind(shch.ScrollDetect.firstEffect), false);
+        shch.ScrollDetect.firstEffect = new shch.ScrollDetect('.aboutus', 'appear', 1);
+        window.addEventListener('scroll', shch.ScrollDetect.firstEffect.scrolling.bind(shch.ScrollDetect.firstEffect), false);
+        shch.ScrollDetect.firstEffect = new shch.ScrollDetect('.keyprinciples', 'appear', 1);
+        window.addEventListener('scroll', shch.ScrollDetect.firstEffect.scrolling.bind(shch.ScrollDetect.firstEffect), false);
     }
 }
 window.addEventListener('load', shch.burger);

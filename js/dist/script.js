1b70ae23eac27cec2435d08cc1909d1b4c9b030b
Genn <intval@i.ua>
2021-04-10 22:37:37 +0300

    Swipe-is-works

--- js/dist/script.js
+++ js/dist/script.js
@@ -31,14 +31,14 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.querySelector(".sphere").appendChild(renderer.domElement);
 
 // Ensure Full Screen on Resize
-function fullScreen() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-window.addEventListener('resize', fullScreen, false)
+// function fullScreen() {
+//     camera.aspect = window.innerWidth / window.innerHeight;
+//     camera.updateProjectionMatrix();
+//
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+// }
+//
+// window.addEventListener('resize', fullScreen, false)
 
 // Scene
 var scene = new THREE.Scene();

ef315e26408f71f9a129bf44c91b9bf09b475dff
Genn <intval@i.ua>
2021-04-16 09:28:07 +0300

    Rename-Classes

--- js/main.js
+++ js/main.js
@@ -93,7 +93,6 @@ let shch = {
 window.addEventListener('load', shch.burger);
 
 shch.foloiButton = function () {
-    console.log(shch['.overno']['.overno'])
     if (shch['.overno']['.overno'].initState) {
         shch['.overno']['.overno'].infoTag.setAttribute('class', shch['.overno']['.overno'].infoTagClass)
         shch['.overno']['.overno'].initState = 0;
@@ -149,7 +148,7 @@ shch.ScrollDetect = function (whoIsAnimate, whatKindAnimate, startChanges) {
 }
 
 shch.sliderDesk = function (selectorSlide, activeForward, activeBack) {
-    this.mum = 0;
+    this.num = 0;
     this.Current = 0;
     this.secondSlide = 1;
     this.thirdSlide = 2;
@@ -167,9 +166,10 @@ shch.sliderDesk = function (selectorSlide, activeForward, activeBack) {
         this.Item[1].setAttribute('class', activeForward + this.num + ' ' + this.Classes);
         this.Item[2].setAttribute('class', activeForward + this.num + ' ' + this.Classes);
         this.num ? this.num = 0 : this.num = 1;
-        this.goVideo();
+        this.goVideo(this.Item[0], this.Current, this.Item[1], this.secondSlide, this.Item[2], this.thirdSlide);
     };
     this.Minus = function () {
+        this.setEmotions = [];
         this.Current -= 1;
         this.secondSlide -= 1;
         this.thirdSlide -= 1;
@@ -180,12 +180,36 @@ shch.sliderDesk = function (selectorSlide, activeForward, activeBack) {
         this.Item[1].setAttribute('class', activeBack + this.num + ' ' + this.Classes);
         this.Item[2].setAttribute('class', activeBack + this.num + ' ' + this.Classes);
         !this.num ? this.num = 1 : this.num = 0;
-        this.goVideo();
+        this.goVideo(this.Item[0], this.Current, this.Item[1], this.secondSlide, this.Item[2], this.thirdSlide);
     }
-    this.goVideo = function () {
-        console.log(this)
+    this.goVideo = function (elem1, numb1, elem2, numb2, elem3, numb3) {
+        shch.getReq('http://localhost:81/emotions/static/1.json', elem1, numb1, elem2, numb2, elem3, numb3);
     }
 };
+
+shch.getReq = function (file, el1, num1, el2, num2, el3, num3) {
+    let http = new XMLHttpRequest();
+    http.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                let emotion = JSON.parse(this.responseText);
+                el1.innerHTML = emotion[num1]["contents"];
+                el2.innerHTML = emotion[num2]["contents"];
+                el3.innerHTML = emotion[num3]["contents"];
+                console.log(el1.innerHTML);
+                console.log(el2.innerHTML);
+                console.log(el3.innerHTML);
+            }
+            if (this.status === 404) {
+                console.log("Page not found.");
+            }
+        }
+    }
+    http.open("GET", file, true);
+    http.send();
+    /* Exit the function: */
+
+}
 
 shch.sliderMob = function (selectorSlide, activeSlide) {
     this.Current = 0;
@@ -255,7 +279,6 @@ shch.RefreshClass = function (childT, newClass, I, button, parentTag, closer) {
                 if (parentTag) this.parentT.setAttribute('class', this.parentTclass + ' ' + this.newClass);
                 this.initState = 1;
             }
-            console.log(this);
         }
     }
 };

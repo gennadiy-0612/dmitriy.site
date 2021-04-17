09f6fe619de2a25f1d0a94d6d9653f66b83a1280
Genn <intval@i.ua>
2021-04-17 17:17:30 +0300

    Slider+Video-static-works

--- js/main.js
+++ js/main.js
@@ -173,34 +173,6 @@ shch.sliderDesk = function (selectorSlide, activeForward, activeBack, setSons) {
     };
 };
 
-shch.includeHTML = function (cb, num) {
-    let z, i, elmnt, file, xhttp;
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        file = elmnt.getAttribute("w3-include-html");
-        if (file) {
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState === 4) {
-                    if (this.status === 200) {
-                        elmnt.innerHTML = this.responseText;
-                    }
-                    if (this.status === 404) {
-                        elmnt.innerHTML = "Page not found.";
-                    }
-                    elmnt.removeAttribute("w3-include-html");
-                    shch.includeHTML(cb);
-                }
-            }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            return;
-        }
-    }
-    if (cb) cb();
-};
-
 shch.emotionsGall = function (papa, sonTag, son, sonSet) {
     let Papa = document.querySelector(papa);
     for (let i = 0; i < sonSet.length; i++) {

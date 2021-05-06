b8b9d4be6cfda2cc933a2e50f0724642dc6b29b2
Genn <intval@i.ua>
2021-05-06 11:02:08 +0300

    Slider+Video-static-works

--- js/dist/script.js
+++ js/dist/script.js
@@ -1,4 +1,10 @@
 shch1 = {};
+if (window.location.origin === 'http://newsite.gk-turan.ru') {
+    shch1.locate = {
+        index1: '/dmitriy.site/',
+        index2: '/index.html'
+    };
+}
 if (window.location.origin === 'https://p.cx.ua') {
     shch1.locate = {
         index1: '/pf/dmitriy.site/',
@@ -15,12 +21,11 @@ if (window.location.origin === 'http://localhost:81') {
 let shchs = {mob: 0, desk: 0, port: 0, land: 0};
 if (window.matchMedia("(max-width: 1070px)").matches) {
     shchs.mob = 1
-    shchs.mini = 300
+    shchs.mini = Math.min(window.innerWidth, window.innerHeight) - 15
 } else {
     shchs.desk = 1
-    if (shchs.desk) shchs.mini = 502
+    if (shchs.desk) shchs.mini = Math.min(window.innerWidth, window.innerHeight) / 2.15
 }
-// Options
 const numberOfParticles = 6000;
 
 const particleImage = './js/dist/particle-tiny.png',
@@ -30,37 +35,15 @@ const particleImage = './js/dist/particle-tiny.png',
 const defaultAnimationSpeed = 1,
     morphAnimationSpeed = 3;
 
-
-// Triggers
-// const triggers = document.getElementsByClassName('triggers')[0].querySelectorAll('span')
-
 var stats = new Stats();
-// stats.showPanel(0);
-// document.body.appendChild( stats.dom );
-
-// Renderer
 var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(shchs.mini, shchs.mini);
 renderer.setClearColor(0x000000, 0); // the default
-document.body.appendChild( renderer.domElement );
 document.querySelector(".back-site__round-in").appendChild(renderer.domElement);
 
-
-// Ensure Full Screen on Resize
-// function fullScreen() {
-//     camera.aspect = window.innerWidth / window.innerHeight;
-//     camera.updateProjectionMatrix();
-//
-//     renderer.setSize(window.innerWidth, window.innerHeight);
-// }
-//
-// window.addEventListener('resize', fullScreen, false)
-
-// Scene
 var scene = new THREE.Scene();
 
-// Camera and position
 var camera = new THREE.PerspectiveCamera(25, 1, 1, 10000);
 if (shchs.mob) {
     if ((window.location.pathname === shch1.locate.index1) || (window.location.pathname === shch1.locate.index2)) {
@@ -68,8 +51,8 @@ if (shchs.mob) {
         camera.position.z = 22;
         console.log('mob sphere')
     } else {
-        camera.position.y = 22;
-        camera.position.z = 33;
+        camera.position.y = 28;
+        camera.position.z = 42;
         console.log('cube mob')
     }
 }
@@ -86,11 +69,6 @@ if (shchs.desk) {
     }
 }
 
-// Lighting
-// var light = new THREE.AmbientLight( 0x404040, 1 ); // soft white light
-// scene.add( light );
-
-// Orbit Controls
 var controls = new THREE.OrbitControls(camera);
 controls.update();
 
@@ -105,8 +83,6 @@ let spherePoints,
 var particles = new THREE.Geometry(),
     sphereParticles = new THREE.Geometry(),
     cubeParticles = new THREE.Geometry()
-// rocketParticles = new THREE.Geometry(),
-// spacemanParticles = new THREE.Geometry();
 
 var pMaterial = new THREE.PointCloudMaterial({
     color: particleColor,
@@ -116,7 +92,6 @@ var pMaterial = new THREE.PointCloudMaterial({
     transparent: true
 });
 
-// Objects
 var geometry = new THREE.SphereGeometry(5, 30, 30);
 
 spherePoints = THREE.GeometryUtils.randomPointsInGeometry(geometry, particleCount)
@@ -125,10 +100,7 @@ var geometry = new THREE.BoxGeometry(9, 9, 9);
 
 cubePoints = THREE.GeometryUtils.randomPointsInGeometry(geometry, particleCount)
 
-// Custom (OGJ) Objects
 
-
-// Particles
 for (var p = 0; p < particleCount; p++) {
     var vertex = new THREE.Vector3();
     vertex.x = 0;
@@ -150,9 +122,6 @@ function createVertices(emptyArray, points, yOffset = 0, trigger = null) {
 
         emptyArray.vertices.push(vertex);
     }
-    // if (trigger !== null) {
-    // 	triggers[trigger].setAttribute('data-disabled', false)
-    // }
 }
 
 var particleSystem = new THREE.PointCloud(
@@ -162,10 +131,8 @@ var particleSystem = new THREE.PointCloud(
 
 particleSystem.sortParticles = true;
 
-// Add the particles to the scene
 scene.add(particleSystem);
 
-// Animate
 const normalSpeed = (defaultAnimationSpeed / 100),
     fullSpeed = (morphAnimationSpeed / 100)
 
@@ -193,7 +160,6 @@ if ((window.location.pathname === shch1.locate.index1) || (window.location.pathn
 }
 
 function toSphere() {
-    // handleTriggers(0);
     morphTo(sphereParticles);
 }
 
@@ -201,16 +167,6 @@ function toCube() {
     // handleTriggers(1);
     morphTo(cubeParticles);
 }
-
-// function toRocket () {
-// 	handleTriggers(2);
-// 	morphTo(rocketParticles);
-// }
-//
-// function toSpaceman () {
-// 	handleTriggers(3);
-// 	morphTo(spacemanParticles);
-// }
 
 function morphTo(newParticles, color = '0xffffff') {
     TweenMax.to(animationVars, .3, {
@@ -232,22 +188,7 @@ function morphTo(newParticles, color = '0xffffff') {
 
 function slowDown() {
     TweenMax.to(animationVars, 4, {
-        ease: Power2.easeOut,
-        speed: normalSpeed, delay: 1
+        ease:
+        Power2.easeOut, speed: normalSpeed, delay: 1
     });
 }
-
-// triggers[0].addEventListener('click', toSphere)
-// triggers[1].addEventListener('click', toCube)
-// triggers[2].addEventListener('click', toRocket)
-// triggers[3].addEventListener('click', toSpaceman)
-//
-// function handleTriggers (disable) {
-// 	for (var x = 0; x < triggers.length; x++) {
-// 		if (disable === x) {
-// 			triggers[x].setAttribute('data-disabled', true)
-// 		} else {
-// 			triggers[x].setAttribute('data-disabled', false)
-// 		}
-// 	}
-// }
